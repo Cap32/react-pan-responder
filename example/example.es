63fa9804931d918f9c3e85e7b3f17add00fb0dce
Webb Peng <webb.png@gmail.com>
2017-07-13 00:49:10 +0800

    update example

--- example/example.es
+++ example/example.es
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import PanResponderView from 'react-pan-responder-view';
 
--- example/index.js
+++ example/index.js
@@ -6,7 +6,7 @@ import { render } from 'react-dom';
 import ReactLogo from './ReactLogo';
 import { AnimatedDiv, AnimatedValueXY, spring } from 'react-web-animated';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/styles';
+import githubGist from 'react-syntax-highlighter/dist/styles/github-gist';
 import exampleCode from './example.es';
 import PanResponderView from 'index';
 
@@ -77,43 +77,79 @@ class App extends Component {
 				</div>
 
 				<div className="doc">
-					<h1>{'<ReactPanResponderView />'}</h1>
+					<h1>ReactPanResponderView</h1>
 					<p>React pan gesture responder component. Just like React Native PanResponder, but for browser</p>
 
 					<h2>Example</h2>
 
-					<SyntaxHighlighter style={docco}>
-						{exampleCode}
-					</SyntaxHighlighter>
+					<figure>
+						<SyntaxHighlighter style={githubGist}>
+							{exampleCode}
+						</SyntaxHighlighter>
+					</figure>
 
 					<h2>Props</h2>
 
-					<table className="props">
-						<tbody>
-							<tr>
-								<th>Property</th>
-								<th>Type</th>
-								<th>Default</th>
-								<th>Required</th>
-								<th>Description</th>
-							</tr>
-							<tr>
-								<td>component</td>
-								<td>React Component</td>
-								<td>{`"div"`}</td>
-								<td>false</td>
-								<td>Wrapper component</td>
-							</tr>
-							<tr>
-								<td>onStartShouldSetPanResponder</td>
-								<td>Function</td>
-								<td>{'() => true'}</td>
-								<td>false</td>
-								<td>Does this view want to become responder on the start of a touch?</td>
-							</tr>
-						</tbody>
-					</table>
+					<div className="props-container">
+						<table className="props">
+							<tbody>
+								<tr>
+									<th>Property</th>
+									<th>Type</th>
+									<th>Default</th>
+									<th>Required</th>
+									<th>Description</th>
+								</tr>
+								<tr>
+									<td>component</td>
+									<td>React Component</td>
+									<td>{`"div"`}</td>
+									<td>false</td>
+									<td>Wrapper component</td>
+								</tr>
+								<tr>
+									<td>onStartShouldSetPanResponder</td>
+									<td>Function</td>
+									<td>{'() => true'}</td>
+									<td>false</td>
+									<td>Does this view want to become responder on the start of a touch?</td>
+								</tr>
+								<tr>
+									<td>onMoveShouldSetPanResponder</td>
+									<td>Function</td>
+									<td>{'() => true'}</td>
+									<td>false</td>
+									<td>Called for every touch move on the View when it is not the responder</td>
+								</tr>
+								<tr>
+									<td>onPanResponderGrant</td>
+									<td>Function</td>
+									<td>{'(ev, gestureState) => {}'}</td>
+									<td>false</td>
+									<td>The View is now responding for touch events. This is the time to highlight and show the user what is happening</td>
+								</tr>
+								<tr>
+									<td>onPanResponderMove</td>
+									<td>Function</td>
+									<td>{'(ev, gestureState) => {}'}</td>
+									<td>false</td>
+									<td>The user is moving their finger</td>
+								</tr>
+								<tr>
+									<td>onPanResponderRelease</td>
+									<td>Function</td>
+									<td>{'(ev, gestureState) => {}'}</td>
+									<td>false</td>
+									<td>{'Fired at the end of the touch, ie "touchUp"'}</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 				</div>
+
+				<footer className="footer">
+					<p>Author: Cap32</p>
+				</footer>
 			</div>
 		);
 	}
