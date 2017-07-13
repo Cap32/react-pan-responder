2faf2a30b14e21da5ee5272731da9a60e0bcbdce
3went2 <webb.png@gmail.com>
2017-07-13 10:42:16 +0800

    publish: add deploy script
    generated from commit 26bf5be07a82fde27038ba46de19d4ebb99f8839

---
+++ ReactLogo.js
@@ -0,0 +1,121 @@
+/* eslint-disable react/no-string-refs */
+
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+const data = {
+	path1: {
+		d: 'M231.7,200c0,17.4-1.7,88-31.7,88s-31.7-70.6-31.7-88s1.7-88,31.7-88S231.7,182.6,231.7,200z'
+	},
+	path2: {
+		d: 'M216.1,227.7c-15,8.9-76.6,43.4-91.9,17.6s44.6-63.2,59.6-72.1s76.6-43.4,91.9-17.6S231.1,218.8,216.1,227.7z'
+	},
+	path3: {
+		d: 'M183.9,227.7c15,8.9,76.6,43.4,91.9,17.6s-44.6-63.2-59.6-72.1s-76.6-43.4-91.9-17.6S168.9,218.8,183.9,227.7z'
+	}
+};
+
+const requestAnimationFrame =
+	global.requestAnimationFrame ||
+	global.webkitRequestAnimationFrame ||
+	global.mozRequestAnimationFrame ||
+	function (callback) {
+		global.setTimeout(callback, 1000 / 60);
+	};
+
+const animate = function (handle) {
+	handle();
+	requestAnimationFrame(() => {
+		animate(handle);
+	});
+};
+
+export default class ReactLogo extends Component {
+	static propTypes = {
+		className: PropTypes.string,
+		style: PropTypes.object,
+		pathStrokeWidth: PropTypes.number,
+		pathFillColor: PropTypes.string,
+		pathStrokeColor: PropTypes.string,
+		bigCircleFillColor: PropTypes.string,
+		smallCircleFillColor: PropTypes.string,
+		bigCircleRadius: PropTypes.number,
+		smallCircleRadius: PropTypes.number,
+		animation: PropTypes.bool,
+		duration: PropTypes.number,
+	};
+
+	static defaultProps = {
+		pathStrokeWidth: 4,
+		pathFillColor: 'none',
+		pathStrokeColor: 'white',
+		bigCircleFillColor: '#61dafb',
+		smallCircleFillColor: '#61dafb',
+		bigCircleRadius: 16,
+		smallCircleRadius: 6,
+		animation: true,
+		duration: 200,
+	};
+
+	state = {
+		length: 0,
+	};
+
+	componentDidMount() {
+		if (this.props.animation) {
+			animate(() => {
+				this.tweenLoop();
+			});
+		}
+
+		Object.keys(data).map(ref => {
+			this[ref] = this.refs[ref];
+		});
+	}
+
+	getPathTotalLength() {
+		return this.pathTotalLength || this.refs.path1.getTotalLength();
+	}
+
+	tweenLoop() {
+		this.current = this.current || 0;
+		this.pathTotalLength = this.getPathTotalLength();
+
+		if (this.current >= this.pathTotalLength) {
+			this.current = 0;
+		}
+		this.current += this.pathTotalLength / this.props.duration;
+		this.setState({
+			length: this.current
+		});
+	}
+
+	getTransforms() {
+		if (!this.refs.path1) {
+			return [];
+		}
+
+		return Object.keys(data).map(ref => {
+			let pos = this[ref].getPointAtLength(this.state.length);
+			return `translate(${pos.x}px, ${pos.y}px)`;
+		});
+	}
+
+	render() {
+		const { className, style, ...props } = this.props;
+		const transforms = this.getTransforms();
+
+		return (
+			<svg viewBox="0 0 400 400" className={className} style={style}>
+				<path ref="path1" fill={props.pathFillColor} stroke={props.pathStrokeColor} strokeWidth={props.pathStrokeWidth} d={data['path1'].d}></path>
+				<path ref="path2" fill={props.pathFillColor} stroke={props.pathStrokeColor} strokeWidth={props.pathStrokeWidth} d={data['path2'].d}></path>
+				<path ref="path3" fill={props.pathFillColor} stroke={props.pathStrokeColor} strokeWidth={props.pathStrokeWidth} d={data['path3'].d}></path>
+				<circle fill={props.bigCircleFillColor} cx="200" cy="200" r={props.bigCircleRadius}></circle>
+				<circle ref="circle1" fill={props.smallCircleFillColor} cx="0" cy="0" r={props.smallCircleRadius} style={{transform: transforms[0], WebkitTransform: transforms[0]}}></circle>
+				<circle ref="circle2" fill={props.smallCircleFillColor} cx="0" cy="0" r={props.smallCircleRadius} style={{transform: transforms[1], WebkitTransform: transforms[1]}}></circle>
+				<circle ref="circle3" fill={props.smallCircleFillColor} cx="0" cy="0" r={props.smallCircleRadius} style={{transform: transforms[2], WebkitTransform: transforms[2]}}></circle>
+			</svg>
+		);
+	}
+}
+
