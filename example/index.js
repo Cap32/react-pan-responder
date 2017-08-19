3aaab8acf971e990d8cb8a69543c081a07b36a0c
Webb Peng <webb.png@gmail.com>
2017-08-20 01:39:49 +0800

    update example

--- example/index.js
+++ example/index.js
@@ -3,12 +3,15 @@
 import './styles.scss';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import ReactLogo from './ReactLogo';
-import { AnimatedDiv, AnimatedValueXY, spring } from 'react-web-animated';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import githubGist from 'react-syntax-highlighter/dist/styles/github-gist';
-import exampleCode from './example.es';
 import PanResponderView from 'index';
+import { Github, User } from 'react-feather';
+import {
+	AnimatedDiv,
+	AnimatedValue,
+	AnimatedValueXY,
+	timing,
+	Easing,
+} from 'react-web-animated';
 
 class Logger extends Component {
 	gestureState = {
@@ -48,7 +51,13 @@ class App extends Component {
 		return false;
 	}
 
-	anim = new AnimatedValueXY();
+	layoutAnim = new AnimatedValueXY();
+
+	styleAnim = new AnimatedValue(0);
+
+	hintAmin = new AnimatedValue(1);
+
+	shouldShowHint = true;
 
 	_handleShouldStartCapture = () => {
 		console.log('onStartShouldSetPanResponderCapture');
@@ -57,7 +66,7 @@ class App extends Component {
 
 	_handleShouldStart = () => {
 		console.log('onStartShouldSetPanResponder');
-		return false;
+		return true;
 	};
 
 	_handleShouldMoveCapture = () => {
@@ -72,12 +81,28 @@ class App extends Component {
 
 	_handleGrant = (ev, gestureState) => {
 		console.log('onPanResponderGrant');
-		const { anim, logger } = this;
-		anim.stopAnimation(({ x, y }) => {
-			anim.setOffset({ x, y });
-			anim.setValue({ x: 0, y: 0 });
-			logger.update(gestureState);
+		const { x0, y0 } = gestureState;
+		const { layoutAnim, styleAnim, hintAmin, logger } = this;
+
+		styleAnim.stopAnimation((value) => {
+			styleAnim.setValue(value);
+			timing(styleAnim, {
+				toValue: 1,
+				duration: 100,
+				easing: Easing.in(Easing.ease),
+			}).start();
 		});
+
+		logger.update(gestureState);
+
+		layoutAnim.stopAnimation(() => {
+			layoutAnim.setValue({ x: x0, y: y0 });
+		});
+
+		if (this.shouldShowHint) {
+			this.shouldShowHint = false;
+			timing(hintAmin, { toValue: 0 }).start();
+		}
 	};
 
 	_handleStart = () => {
@@ -86,8 +111,9 @@ class App extends Component {
 
 	_handleMove = (ev, gestureState) => {
 		console.log('onPanResponderMove');
-		const { anim, logger } = this;
-		anim.setValue({ x: gestureState.dx, y: gestureState.dy });
+		const { moveX, moveY } = gestureState;
+		const { layoutAnim, logger } = this;
+		layoutAnim.setValue({ x: moveX, y: moveY });
 		logger.update(gestureState);
 	};
 
@@ -97,142 +123,64 @@ class App extends Component {
 
 	_handleRelease = (ev, gestureState) => {
 		console.log('onPanResponderRelease');
-		const { anim, logger } = this;
-		anim.flattenOffset();
+		const { styleAnim, logger } = this;
 		logger.update(gestureState);
-		spring(anim, {
+		timing(styleAnim, {
 			toValue: 0,
-			tension: 40,
-			friction: 5,
+			duration: 600,
+			easing: Easing.out(Easing.ease),
 		}).start();
 	};
 
 	render() {
+		const { styleAnim, layoutAnim, hintAmin } = this;
+
 		return (
 			<div className="container">
-				<div className="pan-view">
-					<PanResponderView
-						className="pan"
-						onPanResponderGrant={this._handleGrant}
-						onPanResponderStart={this._handleStart}
-						onPanResponderMove={this._handleMove}
-						onPanResponderRelease={this._handleRelease}
-						onPanResponderEnd={this._handleEnd}
-						onStartShouldSetPanResponderCapture={this._handleShouldStartCapture}
-						onStartShouldSetPanResponder={this._handleShouldStart}
-						onMoveShouldSetPanResponderCapture={this._handleShouldMoveCapture}
-						onMoveShouldSetPanResponder={this._handleShouldMove}
-						component={AnimatedDiv}
-						style={{
-							transform: this.anim.getTranslateTransform(),
-						}}
+				<PanResponderView
+					className="pan-view"
+					onPanResponderGrant={this._handleGrant}
+					onPanResponderStart={this._handleStart}
+					onPanResponderMove={this._handleMove}
+					onPanResponderRelease={this._handleRelease}
+					onPanResponderEnd={this._handleEnd}
+					onStartShouldSetPanResponderCapture={this._handleShouldStartCapture}
+					onStartShouldSetPanResponder={this._handleShouldStart}
+					onMoveShouldSetPanResponderCapture={this._handleShouldMoveCapture}
+					onMoveShouldSetPanResponder={this._handleShouldMove}
+				>
+					<AnimatedDiv
+						className="touch-hint"
+						style={{ opacity: hintAmin }}
 					>
-						<ReactLogo className="logo" />
-					</PanResponderView>
-					<Logger ref={(logger) => (this.logger = logger)} />
-				</div>
-
-				<div className="doc">
-					<h1>ReactPanResponderView</h1>
-					<p>React pan gesture responder component. Just like React Native PanResponder, but for browser</p>
-
-					<h2>Example</h2>
-
-					<figure>
-						<SyntaxHighlighter style={githubGist}>
-							{exampleCode}
-						</SyntaxHighlighter>
-					</figure>
-
-					<h2>Props</h2>
-
-					<div className="props-container">
-						<table className="props">
-							<tbody>
-								<tr>
-									<th>Property</th>
-									<th>Type</th>
-									<th>Default</th>
-									<th>Required</th>
-									<th>Description</th>
-								</tr>
-								<tr>
-									<td>component</td>
-									<td>React Component</td>
-									<td>{`"div"`}</td>
-									<td>false</td>
-									<td>Wrapper component</td>
-								</tr>
-								<tr>
-									<td>onStartShouldSetPanResponder</td>
-									<td>Function | Boolean</td>
-									<td>false</td>
-									<td>false</td>
-									<td>Does this view want to become responder on the start of a touch?</td>
-								</tr>
-								<tr>
-									<td>onStartShouldSetPanResponderCapture</td>
-									<td>Function | Boolean</td>
-									<td>false</td>
-									<td>false</td>
-									<td>Just like `onStartShouldSetPanResponder`, but use capture</td>
-								</tr>
-								<tr>
-									<td>onMoveShouldSetPanResponder</td>
-									<td>Function | Boolean</td>
-									<td>false</td>
-									<td>false</td>
-									<td>Called for every touch move on the View when it is not the responder</td>
-								</tr>
-								<tr>
-									<td>onMoveShouldSetPanResponderCapture</td>
-									<td>Function | Boolean</td>
-									<td>false</td>
-									<td>false</td>
-									<td>Just like `onMoveShouldSetPanResponder`, but use capture</td>
-								</tr>
-								<tr>
-									<td>onPanResponderGrant</td>
-									<td>Function</td>
-									<td>{'(ev, gestureState) => {}'}</td>
-									<td>false</td>
-									<td>The View is now responding for touch events. This is the time to highlight and show the user what is happening</td>
-								</tr>
-								<tr>
-									<td>onPanResponderStart</td>
-									<td>Function</td>
-									<td>{'(ev, gestureState) => {}'}</td>
-									<td>false</td>
-									<td>Called for every touch start when it is the responder</td>
-								</tr>
-								<tr>
-									<td>onPanResponderMove</td>
-									<td>Function</td>
-									<td>{'(ev, gestureState) => {}'}</td>
-									<td>false</td>
-									<td>Called for every touch move when it is the responder</td>
-								</tr>
-								<tr>
-									<td>onPanResponderEnd</td>
-									<td>Function</td>
-									<td>{'(ev, gestureState) => {}'}</td>
-									<td>false</td>
-									<td>Called for every touch end when it is the responder</td>
-								</tr>
-								<tr>
-									<td>onPanResponderRelease</td>
-									<td>Function</td>
-									<td>{'(ev, gestureState) => {}'}</td>
-									<td>false</td>
-									<td>{'Fired at the end of the touch, ie "touchUp"'}</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</div>
+						Touch to Start
+					</AnimatedDiv>
+					<AnimatedDiv
+						className="tracker"
+						style={{
+							opacity: styleAnim,
+							transform: [
+								...layoutAnim.getTranslateTransform(),
+								{
+									scale: styleAnim.interpolate({
+										inputRange: [0, 1],
+										outputRange: [2, 1],
+									}),
+								},
+							],
+						}}
+					/>
+				</PanResponderView>
+				<Logger ref={(logger) => (this.logger = logger)} />
 
 				<footer className="footer">
-					<p>Author: Cap32</p>
+					<a href="https://github.com/Cap32/react-pan-responder-view">
+						<Github size={22} color="white" />
+					</a>
+					<a href="https://github.com/Cap32">
+						<User size={22} color="white" />
+						<span className="author">@Cap32</span>
+					</a>
 				</footer>
 			</div>
 		);
