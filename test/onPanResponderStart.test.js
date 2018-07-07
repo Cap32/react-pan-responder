f1952efbbd42b7ac13e503cb3b0e21b08143beac
Webb Peng <webb.png@gmail.com>
2018-07-08 00:16:12 +0800

    add onPanResponderStart testing

---
+++ test/onPanResponderStart.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PanResponder from '../src';
+import Simulator from './utils/Simulator';
+import mount from './utils/mount';
+
+describe('onPanResponderStart', function () {
+	test('should start on touch start', async () => {
+		const handler = jest.fn();
+		const wrapper = mount(
+			<PanResponder onPanResponderStart={handler} onStartShouldSetPanResponder>
+				{(ref) => <div ref={ref} />}
+			</PanResponder>,
+		);
+		await Simulator.create(wrapper.find(PanResponder).getDOMNode())
+			.touchStart()
+			.touchEnd()
+			.exec();
+		expect(handler).toHaveBeenCalled();
+	});
+
+	test('should start on mouse down', async () => {
+		const handler = jest.fn();
+		const wrapper = mount(
+			<PanResponder onPanResponderStart={handler} onStartShouldSetPanResponder>
+				{(ref) => <div ref={ref} />}
+			</PanResponder>,
+		);
+		await Simulator.create(wrapper.find(PanResponder).getDOMNode())
+			.mouseDown()
+			.mouseUp()
+			.exec();
+		expect(handler).toHaveBeenCalled();
+	});
+
+	test('should start multiple time', async () => {
+		const handler = jest.fn();
+		const wrapper = mount(
+			<PanResponder onPanResponderStart={handler} onStartShouldSetPanResponder>
+				{(ref) => <div ref={ref} />}
+			</PanResponder>,
+		);
+		await Simulator.create(wrapper.find(PanResponder).getDOMNode())
+			.touchStart({}, 1)
+			.touchStart({}, 2)
+			.touchEnd({}, 1)
+			.touchEnd({}, 2)
+			.exec();
+		expect(handler).toHaveBeenCalledTimes(2);
+	});
+});
