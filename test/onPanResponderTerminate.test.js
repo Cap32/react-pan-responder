403cb47b82fd50317ab0d38fdd8e996c0cfde570
3went2 <webb.png@gmail.com>
2018-07-11 16:59:34 +0800

    add onPanResponderTerminate testing

---
+++ test/onPanResponderTerminate.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PanResponder from '../src';
+import Simulator from './utils/Simulator';
+import mount from './utils/mount';
+
+describe('onPanResponderTerminate', function () {
+	test('should not fire `onPanResponderTerminate()` by default', async () => {
+		const handler = jest.fn();
+		let childRef;
+		mount(
+			<PanResponder
+				onStartShouldSetPanResponder={() => true}
+				onPanResponderTerminate={handler}
+				innerRef={(dom) => (childRef = dom)}
+			>
+				{(ref) => <div ref={ref} />}
+			</PanResponder>,
+		);
+		await Simulator.create(childRef)
+			.touchStart()
+			.touchEnd()
+			.exec();
+		expect(handler).toHaveBeenCalledTimes(0);
+	});
+
+	test('should fire `onPanResponderTerminate()` when `onPanResponderTerminationRequest()` returns `true`', async () => {
+		const handler = jest.fn();
+		let childRef;
+		mount(
+			<PanResponder onStartShouldSetPanResponder={() => true}>
+				{(ref) => (
+					<div ref={ref}>
+						<PanResponder
+							onStartShouldSetPanResponder={() => true}
+							onPanResponderTerminationRequest={() => true}
+							onPanResponderTerminate={handler}
+							innerRef={(dom) => (childRef = dom)}
+						>
+							{(ref) => <div ref={ref} />}
+						</PanResponder>
+					</div>
+				)}
+			</PanResponder>,
+		);
+		await Simulator.create(childRef)
+			.touchStart()
+			.touchEnd()
+			.exec();
+		expect(handler).toHaveBeenCalledTimes(1);
+	});
+});
